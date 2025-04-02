eb6230586b00df51ae9f7352d582cd95ec42a3e9
Peter Reutemann <fracpete@gmail.com>
2025-04-03 09:46:55 +1300

    prepared for release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.0.3 (????-??-??)
+0.0.3 (2025-04-03)
 ------------------
 
 - switched to underscores in project name
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "image_dataset_converter",
         "redis",
     ],
-    version="0.0.2",
+    version="0.0.3",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
